aa8b9fd9afc98a69a8f56e5dba3b28f1a0879c9d
SlowRiot <slowriot@voxelstorm.com>
2016-04-11 22:11:36 +0100

    adding forward declaration headers

---
+++ logstorm/logstorm_forward.h
@@ -0,0 +1,20 @@
+#ifndef LOGSTORM_FORWARD_H_INCLUDED
+#define LOGSTORM_FORWARD_H_INCLUDED
+
+namespace logstorm {
+
+namespace sink {
+class base;
+class dummy;
+class stream;
+class console;
+class console_err;
+class fstream;
+class file;
+}
+
+class manager;
+
+}
+
+#endif // LOGSTORM_FORWARD_H_INCLUDED
