6254517cc546f1a2f9c741c302c207af52e813ac
Borealis <slowriot@voxelstorm.com>
2024-11-07 16:05:21 +0000

    Auto-update from libvoxelstorm: remove requirement of cast_if_required, use size_t for sink IDs (7926dea)

--- logstorm/manager.cpp
+++ logstorm/manager.cpp
@@ -1,22 +1,21 @@
 #include "manager.h"
-#include "cast_if_required.h"
 #include "sink/base.h"
 
 namespace logstorm {
 
-unsigned int manager::add_sink(std::shared_ptr<sink::base> newsink) {
+size_t manager::add_sink(std::shared_ptr<sink::base> newsink) {
   /// Add a logging sink, and return its id for later reference
   sinks.emplace_back(newsink);
   sinks.shrink_to_fit();                                                        // we assume that adding sinks is an infrequent operation and minimising over-allocated memory is more important than avoiding reallocations here
-  return cast_if_required<unsigned int>(sinks.size() - 1);
+  return sinks.size() - 1;
 }
 
-std::shared_ptr<sink::base> manager::get_sink(unsigned int sink_id) {
+std::shared_ptr<sink::base> manager::get_sink(size_t sink_id) {
   /// Fetch a logging sink by its id
   return sinks.at(sink_id);
 }
 
-void manager::remove_sink(unsigned int sink_id) {
+void manager::remove_sink(size_t sink_id) {
   /// Remove a logging sink by its id
   if(sink_id >= sinks.size()) {
     return;
